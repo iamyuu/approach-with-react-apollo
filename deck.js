3c376e7bce47668f73802868d1fd89f0b051e8aa
Yusuf <akuyusuf27@gmail.com>
2020-01-27 23:21:12 +0700

    add introduction

--- deck.js
+++ deck.js
@@ -2,6 +2,10 @@ import { nightOwlFull } from 'code-surfer'
 import { customTheme } from './theme'
 import './styles.css'
 
+import { slides as introduction } from './introduction.mdx'
+
 export const themes = [nightOwlFull, customTheme]
 
-export const slides = []
+export const slides = [
+  ...introduction,
+]
