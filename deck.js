8a2a953c398cdcd8173fba56c75a6296a607d128
Yusuf <akuyusuf27@gmail.com>
2020-01-27 23:20:50 +0700

    chore: init

---
+++ deck.js
@@ -0,0 +1,7 @@
+import { nightOwlFull } from 'code-surfer'
+import { customTheme } from './theme'
+import './styles.css'
+
+export const themes = [nightOwlFull, customTheme]
+
+export const slides = []
